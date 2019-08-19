8ce73c040770aed6169ba38ed3e99009c05a494a
admin <admin@DESKTOP-3MO6LL7>
2019-08-19 11:45:21 +0530

    First Commit

---
+++ src/test/java/com/hrm/tests/HRMEX.java
@@ -0,0 +1,18 @@
+package com.hrm.tests;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class HRMEX {
+	
+		@Test
+		public void loginhrm()
+		{
+			System.setProperty("webdriver.chrome.driver", "F:\\chromedriver_win32\\chromedriver.exe");
+			ChromeDriver driver=new ChromeDriver();
+			driver.get("https://www.testingmasters.com/hrm");
+	    }
+
+}
